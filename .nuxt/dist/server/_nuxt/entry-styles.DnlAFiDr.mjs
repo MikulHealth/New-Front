377c243d79a4dc81d14900708bea7df86185d706
Michael Joshua <michaelikejoshua@gmail.com>
2024-04-09 09:59:10 +0100

    update

---
+++ .nuxt/dist/server/_nuxt/entry-styles.DnlAFiDr.mjs
@@ -0,0 +1,2 @@
+import style_0 from './entry-styles-1.mjs-4Uo0_PEg.js';
+export default [style_0]
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,66 @@
+export default {
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DURiBoEN.js",
+    "name": "error-404",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-404.JekaaCis.css": {
+    "file": "error-404.JekaaCis.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DaUrrJNB.js",
+    "name": "error-500",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-500.CNP9nqm1.css": {
+    "file": "error-500.CNP9nqm1.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "D1hE0pzl.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "css": [
+      "entry.CWgSUOzE.css"
+    ],
+    "_globalCSS": true
+  },
+  "entry.CWgSUOzE.css": {
+    "file": "entry.CWgSUOzE.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+}
