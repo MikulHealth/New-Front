a9b0811bcbe7d486b2b90ee04b6c9e846d0eedfc
Michael Joshua <michaelikejoshua@gmail.com>
2024-05-16 09:34:43 +0100

    Update

--- src/components/authLayouts/MobileFooter.js
+++ src/components/authLayouts/MobileFooter.js
@@ -3,9 +3,9 @@ import { Flex, Image, Text, VStack, useDisclosure } from "@chakra-ui/react";
 import HomeIcon from "../../assets/HomeBlack.svg";
 import AppointmentsIcon from "../../assets/AppointmentIcon.svg";
 import WalletIcon from "../../assets/Wallet.svg";
-import SettingsIcon from "../../assets/SettingsIcon.svg"; 
+import SettingsIcon from "../../assets/SettingsIcon.svg";
 import LogoutModal from "../sections/LogoutModal";
-import {NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 const MobileFooter = () => {
   const { isOpen, onClose } = useDisclosure();
   const location = useLocation();
@@ -40,55 +40,94 @@ const MobileFooter = () => {
         paddingY={3}
         paddingX={4}
       >
-        <NavLink to="/dashboard" style={listItemStyle} >
+        <NavLink to="/dashboard" style={listItemStyle}>
           <VStack>
             <Image src={HomeIcon} alt="home" style={iconStyle} />
-            <Text color={location.pathname === "/dashboard" ? "#A210C6" : ""}
-                    fontWeight={
-                      location.pathname === "/dashboard" ? "bold" : ""
-                    }
-                    textDecoration={
-                      location.pathname === "/dashboard" ? "underline" : ""
-                    }>Home</Text>
+            <Text
+              color={location.pathname === "/dashboard" ? "#A210C6" : ""}
+              fontWeight={location.pathname === "/dashboard" ? "bold" : ""}
+              textDecoration={
+                location.pathname === "/dashboard" ? "underline" : ""
+              }
+            >
+              Home
+            </Text>
           </VStack>
         </NavLink>
 
-        <NavLink to="/appointment" style={listItemStyle} activeStyle={activeStyle}>
+        <NavLink
+          to="/appointment"
+          style={listItemStyle}
+          activeStyle={activeStyle}
+        >
           <VStack>
-            <Image src={AppointmentsIcon} alt="appointments" style={iconStyle} />
-            <Text  color={location.pathname === "/appointment" ? "#A210C6" : ""}
-                    fontWeight={
-                      location.pathname === "/appointment" ? "bold" : ""
-                    }
-                    textDecoration={
-                      location.pathname === "/appointment" ? "underline" : ""
-                    }>Appointments</Text>
+            <Image
+              src={AppointmentsIcon}
+              alt="appointments"
+              style={iconStyle}
+            />
+            <Text
+              color={location.pathname === "/appointment" ? "#A210C6" : ""}
+              fontWeight={location.pathname === "/appointment" ? "bold" : ""}
+              textDecoration={
+                location.pathname === "/appointment" ? "underline" : ""
+              }
+            >
+              Appointments
+            </Text>
           </VStack>
         </NavLink>
 
         <NavLink to="/wallet" style={listItemStyle} activeStyle={activeStyle}>
           <VStack>
             <Image src={WalletIcon} alt="wallet" style={iconStyle} />
-            <Text  color={location.pathname === "/wallet" ? "#A210C6" : ""}
-                    fontWeight={
-                      location.pathname === "/wallet" ? "bold" : ""
-                    }
-                    textDecoration={
-                      location.pathname === "/wallet" ? "underline" : ""
-                    }>Wallet</Text>
+            <Text
+              color={location.pathname === "/wallet" ? "#A210C6" : ""}
+              fontWeight={location.pathname === "/wallet" ? "bold" : ""}
+              textDecoration={
+                location.pathname === "/wallet" ? "underline" : ""
+              }
+            >
+              Wallet
+            </Text>
           </VStack>
         </NavLink>
 
         <NavLink to="/settings" style={listItemStyle} activeStyle={activeStyle}>
           <VStack>
             <Image src={SettingsIcon} alt="Settings" style={iconStyle} />
-            <Text  color={location.pathname === "/settings" ? "#A210C6" : ""}
-                    fontWeight={
-                      location.pathname === "/settings" ? "bold" : ""
-                    }
-                    textDecoration={
-                      location.pathname === "/settings" ? "underline" : ""
-                    }>Settings</Text>
+            <Text
+              fontFamily="heading"
+              textDecoration={
+                location.pathname === "/settings" ||
+                location.pathname === "/settings/edit-profile" ||
+                location.pathname === "/settings/change-password" ||
+                location.pathname === "/settings/notification-settings" ||
+                location.pathname === "/settings/help"
+                  ? "underline"
+                  : ""
+              }
+              fontWeight={
+                location.pathname === "/settings" ||
+                location.pathname === "/settings/edit-profile" ||
+                location.pathname === "/settings/change-password" ||
+                location.pathname === "/settings/notification-settings" ||
+                location.pathname === "/settings/help"
+                  ? "bold"
+                  : ""
+              }
+              color={
+                location.pathname === "/settings" ||
+                location.pathname === "/settings/edit-profile" ||
+                location.pathname === "/settings/change-password" ||
+                location.pathname === "/settings/notification-settings" ||
+                location.pathname === "/settings/help"
+                  ? "#A210C6"
+                  : ""
+              }
+            >
+              Settings
+            </Text>
           </VStack>
         </NavLink>
       </Flex>
