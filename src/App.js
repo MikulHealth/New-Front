4463c223d779cfaf4e6eabcc5cb7596892546213
Michael Joshua <michaelikejoshua@gmail.com>
2024-06-30 07:14:26 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <InactivityLogout /> */}
+        <InactivityLogout />
         <Routes>
           <Route path="/" element={<LandingPage />}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
